3f4b30d32cd6fbde3d6227e3494344501ce4914c
jelaB <jela.babic@iten.rs>
2022-11-02 23:42:35 +0100

    spell card initial style

--- src/components/spell/Spell.js
+++ src/components/spell/Spell.js
@@ -9,8 +9,29 @@ const Spell = ({spell}) => {
                 <div className='frame-header'>
                     <h1 className='name'>{spell.name}</h1>
                 </div>
-            </div>
+                <div className="frame-text-box">
 
+                    <p className="description ftb-inner-margin">{spell.desc[0]} </p>
+
+                    <p className="description">{spell.higher_level[0]}</p>
+
+                    <p className="flavour-text">{spell.material}</p>
+
+                </div>
+
+                <div className="frame-bottom-info inner-margin">
+                    <div className="fbi-left">
+                        <p>Range: {spell.range}</p>
+                        <p>Duration: {spell.duration}</p>
+                    </div>
+
+                    <div className="fbi-center"> FAV</div>
+
+                    <div className="fbi-right">
+                        Components: {spell.components}
+                    </div>
+                </div>
+            </div>
         </div>
     </div>);
 }
--- src/components/spell/SpellDetail.js
+++ src/components/spell/SpellDetail.js
@@ -3,7 +3,6 @@ import {useLocation} from "react-router-dom";
 import {getSpellByID} from "../../services/DndService";
 import Spell from "./Spell";
 import "./card.css"
-import {handleError} from "../../services/UtilsService";
 
 
 function SpellDetail(props) {
@@ -11,11 +10,10 @@ function SpellDetail(props) {
     const location = useLocation();
 
     useEffect(() => {
-        getSpellByID(location.state.url).then(details =>{  setSpellDetails(details)});
+        getSpellByID(location.state.url).then(details =>  setSpellDetails(details));
     }, [location.state.url])
 
 
-    console.log(spellDetails);
     return (<Spell spell={spellDetails}></Spell>);
 
 }
